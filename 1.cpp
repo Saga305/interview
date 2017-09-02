9fb8a33ad4b7dc65306318ce55a111c3d85f3b8e
ajaypatel305 <ajay.patel305@gmail.com>
2017-09-02 21:41:19 +0530

    Practice program.

---
+++ 1.cpp
@@ -0,0 +1,31 @@
+//what will be the output of the following program.Uncomment the prints, compile and run it for better understanding.
+
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	int *ptr;
+	ptr = (int*) malloc(3*sizeof(int));
+
+	if(!ptr)
+	{
+		cout << "Memory Allocation Failed";
+		exit(1);
+	}
+	
+#if 0
+	for (int i = 0; i < 3 ; i++)
+	{
+		printf("Address of ptr[%d] = %p\n",i,(ptr +i)); 
+	}
+#endif
+
+	ptr = ptr + 1;
+//	printf("After ptr++ address of ptr = %p\n",ptr); 
+
+	free(ptr);
+
+	return 0;
+}
