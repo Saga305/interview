8e99b0e3348bb832bcdf59afe8775207ed93c084
Saga305 <ajay.patel305@gmail.com>
2018-05-22 20:23:48 +0530

    Quick sort

---
+++ quicksort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int a[] = {10,9,8,7,6,5,11};
+
+int partition(int l,int h)
+{
+    int pivot = a[l];
+    int i = l;
+    int j = h;
+    do
+    {
+       i++;
+    }while (a[i] <= pivot);
+    do
+    {
+        j--;
+    }while (a[j] > pivot);
+    
+    if (i < j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
+    {
+      int temp = a[l];
+        a[l] = a[j];
+        a[j] = temp;
+        
+    }
+    return j;
+}
+
+void quicksort(int l,int h)
+{
+    if(l < h)
+    {
+    int j = partition(l,h);
+    quicksort(l,j);
+    quicksort(j+1,h);
+    }
+    return;
+}
+
+int main() {
+
+    quicksort(0,(sizeof(a)/sizeof(a[0])));
+    
+    for(int i = 0; a[i]; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
